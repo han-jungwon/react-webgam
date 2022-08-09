2df78e5799cd2535aa45afc6da6b632912ac7499
jungwon <rhehf1024@naver.com>
2022-08-09 16:37:35 +0900

    모듈 시스템과 웹팩 설정

---
+++ src/2.끝말잇기/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', // 실서비스 : production
+    devtool: 'eval',
+
+    entry: {
+        app: ['./client'],
+    }, // 입력
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js'
+    }, // 출력
+};
