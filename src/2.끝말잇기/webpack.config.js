7e378859114d3c1a199cef5a55e4f65cbddbd785
jungwon <rhehf1024@naver.com>
2022-08-12 05:13:46 +0900

    컨트롤드 인풋 vs 언컨트롤드 인풋

--- src/2.끝말잇기/webpack.config.js
+++ src/2.끝말잇기/webpack.config.js
@@ -4,10 +4,14 @@ module.exports = {
     name: 'wordrelay-setting',
     mode: 'development', // 실서비스 : production
     devtool: 'eval',
+    resolve: {
+      extensions: ['.js,', 'jsx']
+    },
 
     entry: {
         app: ['./client'],
     }, // 입력
+    modules:[],
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'app.js'
